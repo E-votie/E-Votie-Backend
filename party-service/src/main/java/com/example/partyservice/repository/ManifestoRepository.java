1812f9554fdd176368e9841eeee6b29c8c73bab8
lahiru Jayathilake <lahiruthpala@gmail.com>
2024-08-07 06:07:21 +0530

    Initialization

--- party-service/src/main/java/com/example/partyservice/repository/ManifestoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.partyservice.repository;
-
-import com.example.partyservice.model.Manifesto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManifestoRepository extends JpaRepository<Manifesto, Integer> {
-}
