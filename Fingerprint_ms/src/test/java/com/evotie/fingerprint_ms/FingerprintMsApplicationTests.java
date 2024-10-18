1e784dd6832c58524487559057e543cbc193c342
lahiru Jayathilake <lahiruthpala@gmail.com>
2024-10-18 23:43:37 +0530

    fix

---
+++ Fingerprint_ms/src/test/java/com/evotie/fingerprint_ms/FingerprintMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evotie.fingerprint_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingerprintMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
