54859c3d6e0e22f6b13bdf2290f5f1406ca53675
E.A.P.P.EDIRISOORIYA <121412462+pramith-7@users.noreply.github.com>
2024-12-02 01:30:52 +0530

    office portal service created

---
+++ officeportal/src/main/java/com/evotie/officeportal/OfficeportalApplication.java
@@ -0,0 +1,13 @@
+package com.evotie.officeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeportalApplication.class, args);
+	}
+
+}
---
+++ officeportal/src/test/java/com/evotie/officeportal/OfficeportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evotie.officeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
